9af351b09c006c5394b369b4ece61fd17c6f90c8
mkolotovich <k2punk@gmail.com>
2020-08-27 17:50:31 +0300

    finish styling calendar block

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -1,47 +1,35 @@
-// import Lightpick from 'lightpick';
-// var picker = new Lightpick({ 
-//   field: document.getElementById('datepicker1'),
-//   singleDate: false,
-//   lang: 'ru',
-//   footer: true,
-//   inline: true,
-//   locale: {
-//     buttons: {
-//       prev: 'arrow_backward',
-//       next: 'arrow_forward',
-//       close: '×',
-//       reset: 'очистить',
-//       apply: 'применить'
-//     }
-//   },
-//   startDate: '2019-08-19',
-//   endDate: '2019-08-23',
-// });
-//
-// var title = document.querySelector('.lightpick__month-title');
-// var toolbar = document.querySelector('.lightpick__toolbar');
-// toolbar.appendChild(title);
-// var next = document.querySelector('.lightpick__next-action');
-// next.classList.add('material-icons');
 import flatpickr from "flatpickr";
 import { Russian } from "flatpickr/dist/l10n/ru.js"
-var months = document.querySelector('.flatpickr-monthDropdown-months');
 let calendar = flatpickr('#flat', {
   locale: Russian, // locale for this instance only
   mode: "range",
   defaultDate: ["2019-08-19", "2019-08-23"],
   inline: true,
   nextArrow: '<i class="material-icons">arrow_forward</i>',
-  prevArrow: '<i class="material-icons">arrow_backward</i>'
+  prevArrow: '<i class="material-icons">arrow_backward</i>',
+  monthSelectorType: "static",
+  onYearChange: function(selectedDates, dateStr, instance) {
+    if (calendar.currentYear > 2019) {
+      year.style.width = 49 + 'px';
+    } else if (calendar.currentYear <= 2019) {
+      year.style.width = 44 + 'px';
+    }
+  }
 });
+var year = document.querySelector('.flatpickr-current-month .numInputWrapper');
+var calendarBlock = document.querySelector('.flatpickr-calendar.inline');
+var buttons = document.querySelector('.buttons-list');
+calendarBlock.append(buttons);
+var augustDays = document.querySelectorAll('.rangeMode .flatpickr-day:nth-child(n+36)');
+var DaysInRange = document.querySelectorAll('.flatpickr-day.inRange').length - 1;
+var lastDayInRange = document.querySelectorAll('.flatpickr-day.inRange')[DaysInRange];
 var today = document.querySelector('.today');
 if (today === null) {
   document.querySelector('.flatpickr-day[aria-label="Август 8, 2019"]').classList.add('today');
 }
-// calendar.onMonthChange(function([], dateStr, instance) {
-//   if (calendar.currentMonth === 7 && calendar.currentYear === 2019) {
-//     months.style.width = 67 + 'px';
-//   } else {
-//     months.style.width = 'auto';
-//   }
-// });
+if (calendar.currentMonth === 7 && calendar.currentYear === 2019) {
+  for(let i = 0; i < augustDays.length; i++) {
+    augustDays[i].style.display = 'none';
+  }
+};
+lastDayInRange.style.boxShadow = '0px 0 0 #bc9cff40, 19px 0 0 #bc9cff40';
