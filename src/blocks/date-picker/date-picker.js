8b5009f01f140fb88fec95ca2e9fb3aa9becf678
mkolotovich <k2punk@gmail.com>
2020-09-23 17:55:53 +0300

    finish pixel perfect cards page

--- src/blocks/date-picker/date-picker.js
+++ src/blocks/date-picker/date-picker.js
@@ -2,4 +2,7 @@ import $ from 'jquery';
 import '../date-picker/jquery-ui';
 $( function() {
   $( "#datepicker" ).datepicker();
+  $( "#datepicker1" ).datepicker();
+  $( "#datepicker2" ).datepicker();
+  $( "#datepicker3" ).datepicker();
 } );
--- src/blocks/preview-text-field/text-field.js
+++ src/blocks/preview-text-field/text-field.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'select2';
 
 $(document).ready(function() {
-  $('.js-example-basic-single').select2();
+  $('.js-example-basic-multiple').select2();
 
   function formatState (state) {
     if (!state.id) {
@@ -10,18 +10,15 @@ $(document).ready(function() {
     }
     var $state;
     if (state.id === 'применить') {
-      console.log(state.id);
       $state = $(
         '<button class="dropdown__apply">' + state.text + '</button>'
       );  
     } else if(state.id === 'очистить') {
-      console.log(state.id);
       $state = $(
         '<button class="dropdown__apply">' + state.text + '</button><button class="dropdown__apply dropdown__apply_cancel">' + state.id + '</button>'
       ); 
     }
     else { 
-      console.log(state.id);
       $state = $(
         '<div class="dropdown"><button class="dropdown__button" />-</button><span>' + state.id + '</span><button class="dropdown__button" />+</button></div>' + state.text + '</li>'
       );
@@ -29,21 +26,35 @@ $(document).ready(function() {
     return $state;
   };
   var dropdown = $(".preview-text-field__item_open .preview-text-field__item-input");
+  //
+  var dropdownIncrease = $("body > span:nth-child(5) .select2-results__options li:nth-child(1) .dropdown .dropdown__button:last-child");  
+  var parent = dropdownIncrease.parentNode;
+  //
   dropdown.select2({
     placeholder: "2 спальни, 2 кровати...",
-    templateResult: formatState
+    templateResult: formatState,
   });
-  
+  //
+  dropdownIncrease.onclick = function() {
+    var count = parent.lastElementChild.textContent;
+    console.log(count);
+    parent.lastElementChild.textContent = count + 1;
+  };
+  //
   var dropdownApply = $(".container_ui-kit .form-elements-center-column .preview-text-field__item-input");
-
+  var multiple = $(".container_ui-kit > ul:nth-child(1) > li:nth-child(3) > select");
   var dropdownClear = $(".container_ui-kit .form-elements-right-column .preview-text-field__item-input");
   
-  dropdownClear.select2({
-    placeholder: "3 гостя",
+  multiple.select2({
+    placeholder: "Сколько гостей",
     templateResult: formatState
   });
   dropdownApply.select2({
     placeholder: "Сколько гостей",
+    templateResult: formatState
+  });
+  dropdownClear.select2({
+    placeholder: "3 гостя",
     templateResult: formatState
   });
   dropdown.select2('open');
